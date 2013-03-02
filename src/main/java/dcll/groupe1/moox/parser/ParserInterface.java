eaa8ddd8cbed0cee9d0c0842045a62347c5ae18c
SERIN Kevin <lepiaf31@gmail.com>
2013-03-02 15:11:31 +0100

    #2 Creation des interfaces pour la generation et le parsage de code

---
+++ src/main/java/dcll/groupe1/moox/parser/ParserInterface.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package dcll.groupe1.moox.parser;
+
+import java.net.URI;
+
+import dcll.groupe1.moox.domain.Tag;
+
+/**
+ * Interface for implementations of parsers.
+ * @author SERIN Kévin
+ *
+ */
+public interface ParserInterface {
+	
+	/**
+	 * Parse a document.
+	 * @param uri The URI of the document.
+	 * @return The root tag of the document.
+	 * @throws ParserException If an error occurred during the parsing.
+	 */
+	public Tag parse(URI uri) throws ParserException;
+}
