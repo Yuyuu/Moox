ab028fe8ed9d407ef5273264d1deb136ca1b593b
Jeremy Simar <exasky@live.fr>
2013-03-05 22:29:59 +0100

    Modification de App.java car modification du fichier Parser en XmlParser.java

--- src/main/java/dcll/groupe1/moox/App.java
+++ src/main/java/dcll/groupe1/moox/App.java
@@ -3,8 +3,9 @@ package dcll.groupe1.moox;
 import java.io.File;
 import java.net.URI;
 
+import dcll.groupe1.moox.GUI.GUI;
 import dcll.groupe1.moox.parser.ParserException;
-import dcll.groupe1.moox.parserImpl.Parser;
+import dcll.groupe1.moox.parserImpl.XmlParser;
 
 /**
  * Hello world!
@@ -14,10 +15,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        Parser pars = new Parser();
+        XmlParser pars = new XmlParser();
         File f = new File("MoodleXML.xml");
         URI uri = f.toURI();
-        
+        new GUI();
         
         try {
 			pars.parse(uri);
