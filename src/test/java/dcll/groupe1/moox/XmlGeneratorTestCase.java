c306292c39e18e6592fa4ae4c06e10923eb72759
LWAUPS <l.warin@sfr.fr>
2013-03-07 22:50:29 +0100

    #12 Ajout d'un test unitaire pour le générateur XML

--- src/test/java/dcll/groupe1/moox/XmlGeneratorTestCase.java
+++ src/test/java/dcll/groupe1/moox/XmlGeneratorTestCase.java
@@ -1,15 +1,25 @@
 package dcll.groupe1.moox;
 
 import java.io.BufferedWriter;
+import java.io.StringWriter;
 
 import java.io.File;
 
 import java.io.FileWriter;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import junit.framework.TestCase;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.Document;
 
 import dcll.groupe1.moox.domain.Tag;
 import dcll.groupe1.moox.generator.impl.XmlGenerator;
@@ -32,8 +42,13 @@ public class XmlGeneratorTestCase extends TestCase {
 		}
 	}
 
+	/**
+	 * Test of XmlGenerator in parsing a Tag and then generating it
+	 * 
+	 * @throws Exception
+	 */
 	@Test
-	public void testParser() throws Exception {
+	public void testGenerator1() throws Exception {
 		// On genere un fichier Xml à partir du Tag
 		XmlGenerator xml = new XmlGenerator();
 		FileWriter fw = new FileWriter("toto.xml", false);
@@ -54,5 +69,31 @@ public class XmlGeneratorTestCase extends TestCase {
 
 		// Comparaison entre le Tag initial et le Tag issu de la génération
 		assertEquals(tag, test);
+	}
+
+	/**
+	 * Test of XmlGenerator with String representation of Xml files
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testGenerator2() throws Exception {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document doc = builder.parse(f);
+
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer transformer = tf.newTransformer();
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		StringWriter writer = new StringWriter();
+		transformer.transform(new DOMSource(doc), new StreamResult(writer));
+		String output = writer.getBuffer().toString().replaceAll("\\s", "");
+
+		XmlGenerator xml = new XmlGenerator();
+		String tmp = xml.generate(tag).replaceAll("\\s", "");
+
+		// suppression de l'en-tête du fichier XML
+		String test = tmp.substring(tmp.indexOf("<quiz>"));
+		assertEquals(output, test);
 	}
 }
