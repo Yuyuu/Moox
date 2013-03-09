38b2c5b37f3d9745f91952b79f500597cdbc8c2e
LWAUPS <l.warin@sfr.fr>
2013-03-09 16:39:27 +0100

    #4 Implémentation du ParserInterface

--- src/main/java/dcll/groupe1/moox/parser/impl/JsonParser.java
+++ src/main/java/dcll/groupe1/moox/parser/impl/JsonParser.java
@@ -1,98 +1,31 @@
 package dcll.groupe1.moox.parser.impl;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URI;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
 import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import dcll.groupe1.moox.domain.Attribute;
 import dcll.groupe1.moox.domain.Tag;
+import dcll.groupe1.moox.parser.ParserException;
+import dcll.groupe1.moox.parser.ParserInterface;
 
-public class JsonParser {
+public class JsonParser implements ParserInterface {
 
-	public static void main(String args[]) {
-		InputStream f;
+	@Override
+	public Tag parse(URI uri) throws ParserException {
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode tree;
+		Tag tag = new Tag();
 		try {
-			f = new FileInputStream("testJson.json");
-			ObjectMapper mapper = new ObjectMapper();
-			JsonNode tree = mapper.readTree(f);
-
-			Iterator<Entry<String, JsonNode>> p = tree.getFields();
-
-			Tag tag = new Tag();
-			while (p.hasNext()) {
-				Entry<String, JsonNode> node = p.next();
-				JsonNode value = node.getValue();
-				// si non { ou [
-				if (!value.isContainerNode()) {
-					tag.setValue(value.asText());
-				} else if (value.isArray()) {
-					System.out.println("*ARRAY : " + value);
-					// tableau d'attributs ou de subtags
-					// node = value.getFields().next();
-					switch (node.getKey()) {
-					case "attributes":
-						System.out.println("go att");
-						Iterator<JsonNode> att = value.getElements();
-						while (att.hasNext()) {
-							System.out.println("attribuuuuuuuuts");
-							JsonNode attribut = att.next();
-							Entry<String, JsonNode> toto = attribut.getFields().next();
-							Attribute attribute = new Attribute();
-							attribute.setName(toto.getKey());
-							attribute.setValue(toto.getValue().asText());
-							System.out.println(attribute);
-							tag.addAttribute(attribute);
-						}
-
-						// System.out.println("attributes " +
-						// node.getValue().getFieldNames());
-						break;
-					case "subTags":
-						System.out.println("go sub");
-						break;
-					}
-				} else if (value.isObject()) {
-					// c'est un Tag
-					tag.setName(node.getKey());
-				}
-
-				// System.out.println("***" + value.getFields().hasNext());
-				if (!p.hasNext() && value.getFields().hasNext()) {
-					p = value.getFields();
-				}
-
-			}
-
-			// System.out.println(tag.getName());
-			// System.out.println(tag.getValue());
-			// Iterator<String> it = tree.getFieldNames();
-			// while(it.hasNext()) {
-			// System.out.println(it.next());
-			// }
-
-			/*
-			 * Tag tag = mapper.convertValue(tree.get("quiz"), ref);
-			 * System.out.println(tag.getName());
-			 * System.out.println(tag.getValue());
-			 * System.out.println(tag.getAttributes());
-			 * System.out.println(tag.getSubTags().get(0).getName());
-			 * System.out.println(tag.getSubTags().get(0).getValue());
-			 */
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
+			tree = mapper.readTree(new File(uri));
+			tag = parseTag(tree);
+		} catch (JsonProcessingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -100,5 +33,69 @@ public class JsonParser {
 			e.printStackTrace();
 		}
 
+		return tag;
 	}
+
+	private Tag parseTag(JsonNode nodeToParse) {
+		Tag t = new Tag();
+		Iterator<Entry<String, JsonNode>> p = nodeToParse.getFields();
+		while (p.hasNext()) {
+			Entry<String, JsonNode> node = p.next();
+			JsonNode value = node.getValue();
+
+			// si non { ou [
+			if (!value.isContainerNode()) {
+				t.setValue(value.asText());
+			} else if (value.isArray()) {
+				// tableau d'attributs ou de subtags
+				switch (node.getKey()) {
+				case "attributes":
+					Iterator<JsonNode> att = value.getElements();
+					while (att.hasNext()) {
+						JsonNode attribut = att.next();
+						Entry<String, JsonNode> toto = attribut.getFields()
+								.next();
+						Attribute attribute = new Attribute();
+						attribute.setName(toto.getKey());
+						attribute.setValue(toto.getValue().asText());
+						t.addAttribute(attribute);
+					}
+					break;
+				case "subTags":
+					Iterator<JsonNode> sub = value.getElements();
+					while (sub.hasNext()) {
+						JsonNode subTag = sub.next();
+						t.addTag(parseTag(subTag));
+					}
+					break;
+				}
+			} else if (value.isObject()) {
+				t.setName(node.getKey());
+			}
+
+			if (!p.hasNext() && value.getFields().hasNext()) {
+				p = value.getFields();
+			}
+
+		}
+
+		System.out.println("**Name : " + t.getName());
+		System.out.println("**Value : " + t.getValue());
+		System.out.println(t.getAttributes());
+		// System.out.println(tag.getSubTags().get(0).getName());
+		// System.out.println(tag.getSubTags().get(0).getValue());
+		return t;
+	}
+
+	public static void main(String args[]) {
+		File f = new File("testJson.json");
+		JsonParser jsp = new JsonParser();
+		try {
+			System.out.println(jsp.parse(f.toURI()));
+		} catch (ParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
